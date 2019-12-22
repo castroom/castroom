d17839c18a691af553cda28294ff05e7df5928ca
Maharsh Patel <patel@maharsh.net>
2019-12-22 01:40:55 -0700

    refactor using providers for loose coupling

--- discovery/index.js
+++ discovery/index.js
@@ -1,50 +1,25 @@
 import axios from 'axios';
+import iTunesProvider from "./providers/iTunes";
 
-var urlFromQueue = "https://podcasts.apple.com/us/genre/podcasts-arts/id1301";
+var url = "https://podcasts.apple.com/us/genre/podcasts-arts/id1301";
 
-const getClickableUrls = (pageData) => {
-  var clickableUrls = pageData.match(/https:\/\/podcasts.apple.com\/\w{2}\/((genre\/podcasts-.+?\/id\d{4}(\?letter=.(\&amp;page=\d\d?#page)?)?)|podcast\/(((\d+|\w+)\-){0,100}(\d+|\w+)\/id\d{0,12})(?="))/g);
+var provider = new iTunesProvider();
 
-  if (clickableUrls && clickableUrls.length >= 0) {
-    // prevent the link back to this page from being processed again
-    clickableUrls = clickableUrls.filter(item => item !== urlFromQueue)
+axios.get(url).then(response => {
+  const crawlableUrls = provider.getCrawlableUrls(response.data, url);
+  console.log(crawlableUrls)
 
-    return Array.from(new Set(clickableUrls));
-  }
-
-  return [];
-}
-
-const getID = (url) => {
-  var match = url.match(/\/id\d*/g)[0];
-
-  if (match.length > 8) {
-    match = match.replace("/id", "");
-    return match;
-  }
-  return null;
-}
-
-
-axios.get(urlFromQueue).then(response => {
-  // find all the outgoing urls
-  const outgoingUrls = getClickableUrls(response.data)
-  console.log(outgoingUrls)
-
-  outgoingUrls.forEach(url => {
+  crawlableUrls.forEach(url => {
     // TODO: add this url to the queue - could also do a batch update
     console.log("Adding", url, "to the queue");
   });
 
-  var id = getID(urlFromQueue);
+  var id = provider.getPodcastId(url);
   if (id !== null) {
-    // TODO: lookup the new item 
-    console.log("Lookup", urlFromQueue);
+    var metadata = provider.lookup(id)
+    console.log(metadata);
+    // TODO: save it to ElasticSearch here
   }
-
-  
-
-  // if this points to a podcast url -> look up the metadata and save it to ES
   
 }).catch(error => {
   // save the message back in the queue so that we can go over it at some point
