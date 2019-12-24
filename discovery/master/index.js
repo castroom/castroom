fe3385cd246dfbf395ca4b241e912981c49977f1
Maharsh Patel <patel@maharsh.net>
2019-12-23 18:23:49 -0700

    add submit endpoint with basic logic

--- discovery/master/index.js
+++ discovery/master/index.js
@@ -1,16 +1,44 @@
-const express = require('express');
+import express from 'express';
+import levelup from 'levelup';
+import leveldown from 'leveldown';
+
+// TODO: clear out the old database here
+var cache = levelup(leveldown("./cache"))
+// var buffer = [];
+
 const app = express();
 
+app.get('/submit', (req, res) => {
+    var key = "test";
+    var value = "test_val";
 
-app.get('/', (req, res) => {
-  res.send('Crawler\n');
+    // end the connection immediately since we have nothing to send back to the workers
+    res.status(200).send(); 
+
+    cache.get(key, (gerr) => {
+        if (gerr) {
+            // not found in cache
+            console.log(gerr);
+
+            cache.put(key, value, (perr) => {
+                if (perr) {
+                    console.log(perr);
+                } else {
+                    console.log(key, "was pushed");
+                }
+            });
+        } else {
+            // already exists in cache
+            console.log(key, "already exists");
+        }
+    });
+    
+    
 });
-
-
 
 // Start the server
 const PORT = 8080;
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
 });
