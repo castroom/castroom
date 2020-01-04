0ac7c32bbda2d09a07048f5ca08021406f737055
Maharsh Patel <patel@maharsh.net>
2020-01-04 00:31:26 -0700

    initial commit for website

---
+++ web/src/components/Website.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Website.css";
+
+function Website() {
+  return (
+    <div className="Website">
+      Testing
+    </div>
+  );
+}
+
+export default Website;
---
+++ web/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import Website from './components/Website';
+
+ReactDOM.render(<Website />, document.getElementById('root'));
