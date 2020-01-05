89fffde63ba3628a146aa74cf0c96e6a1d47df6c
Maharsh Patel <patel@maharsh.net>
2020-01-04 18:25:01 -0700

    install helmet and cors support

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import helmet from "helmet";
+import cors from "cors";
+
+const corsOptions = {
+  origin: "https://castroom.co",
+};
 
 const app = express();
 app.use(bodyParser.json());
+app.use(helmet()); // sets security related HTTP response headers
 
-app.get("/", (req, res) => {
+app.get("/", cors(corsOptions), (req, res) => {
   res.send({ test: "testing" });
 });
 
