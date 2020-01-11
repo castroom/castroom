a361ed419e70b999110c9a6d1d9303431499a1b7
Maharsh Patel <patel@maharsh.net>
2020-01-11 02:22:12 -0700

    add feed retrieval endpoint

--- api/src/index.js
+++ api/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 import cors from "cors";
+import axios from "axios";
 import ElasticsearchService from "./services/ElasticsearchService";
 
 const whitelist = ["https://castroom.co", "http://localhost:3000", "https://castroom.web.app", "https://castroom.firebaseapp.com"];
@@ -21,13 +22,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(helmet()); // sets security related HTTP response headers
 
-app.get("/", cors(corsOptions), async (req, res) => {
+app.get("/", cors(corsOptions), (req, res) => {
   if (req && req.query.q) {
     es.search("trackName", req.query.q, 5).then((response) => {
       console.log(req.query.q);
       res.send(response);
     }).catch((err) => {
       console.log(err);
+    });
+  } else {
+    res.sendStatus(200);
+  }
+});
+
+app.get("/feed", cors(corsOptions), async (req, res) => {
+  if (req && req.query.url) {
+    axios.get(req.query.url).then((response) => {
+      console.log(response.data);
+      res.send(response.data);
+    }).catch((err) => {
+      console.log(err);
+      res.sendStatus(404);
     });
   } else {
     res.sendStatus(200);
