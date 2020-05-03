303b9b1d013e8eb98497b030bad5082803e1987f
Maharsh Patel <patel@maharsh.net>
2020-05-02 20:27:56 -0600

    add notice to website

--- web/src/components/Website.js
+++ web/src/components/Website.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Search from "./Search";
 import "../styles/Website.scss";
 
@@ -9,6 +10,9 @@ class Website extends Component {
   render() {
     return (
       <Container className="website">
+        <Col md={12} className="notice">
+          The podcast index was last updated January 2020. Please allow a few seconds for the first search result.
+        </Col>
         <Row>
           <Search/>
         </Row>
